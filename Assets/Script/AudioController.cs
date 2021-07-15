ac2f66970df45ee296c5b2c1d35c4d68cc11b96e
LacJayce092 <thong.phan092@gmail.com>
2021-07-15 14:37:36 +0700

    Update Audio

---
+++ Assets/Script/AudioController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class AudioController : MonoBehaviour
+{
+    public static AudioController aud;
+ 
+    public AudioSource musicAud;
+    
+    public AudioSource soundAud;
+    public AudioClip win;
+    public AudioClip rightAnswer;
+    public AudioClip wrongAnswer;
+    private void Awake()
+    {
+        makeSingleton();
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void makeSingleton()
+    {
+        if (aud == null)
+            aud = this;
+        else
+            Destroy(gameObject);
+    }
+    void PlaySound(AudioClip sound)
+    {
+        if(soundAud && sound)
+        {
+            soundAud.PlayOneShot(sound);
+        }
+    }
+    public void PlayCorrectSound()
+    {
+        PlaySound(rightAnswer);
+    }
+    public void PlayLoseSound()
+    {
+        PlaySound(wrongAnswer);
+    }
+    public void PlayWinSound()
+    {
+        PlaySound(win);
+    }
+}
